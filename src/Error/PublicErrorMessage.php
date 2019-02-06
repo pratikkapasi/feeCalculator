08030552fd0a76bc737ce052d73ccd6f7656beae
Pratik Kapasi <pratik.kapasi@razorpay.com>
2019-02-07 03:02:36 +0530

    [error] adds a new error message

--- src/Error/PublicErrorMessage.php
+++ src/Error/PublicErrorMessage.php
@@ -6,6 +6,7 @@ use Lendable\Interview\Interpolation\Service\Fee\Structure as Struct;
 
 class PublicErrorMessage
 {
-    const TERM_INVALID      = 'The term provided is invalid';
-    const AMOUNT_INVALID    = 'The amount provided should be between ' . Struct::MIN_AMOUNT_ALLOWED . ' and ' . Struct::MAX_AMOUNT_ALLOWED;
+    const TERM_INVALID          = 'The term provided is invalid';
+    const AMOUNT_INVALID        = 'The amount provided should be between ' . Struct::MIN_AMOUNT_ALLOWED . ' and ' . Struct::MAX_AMOUNT_ALLOWED;
+    const INTERPOLATION_FAILURE = 'Interpolation failed';
 }
